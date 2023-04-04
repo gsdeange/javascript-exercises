450f008cb496322c922e127b0ec156151fa685b4
gsdeange <garrettdeangelis@gmail.com>
2023-04-04 15:55:42 -0700

    Finished some exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+   let array = [];
+   for(i = 0; i<s.length; i++){
+      array.push(s[i]);
+   }
+   return array.reverse().toString().replaceAll(",", "");
 };
 
 // Do not edit below this line
